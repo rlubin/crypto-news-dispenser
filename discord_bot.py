3bc309d661df66a2ad3d270198a1346fae05c580
Ryan Lubin <ryan.lubin1@gmail.com>
2023-12-28 14:11:15 -0500

    setup windows task scheduler with script

--- discord_bot.py
+++ discord_bot.py
@@ -1,8 +1,7 @@
 import os
 from dotenv import load_dotenv
 import discord
-import asyncio
-import scrape_manager
+import scrappers.scrape_manager as scrape_manager
 
 load_dotenv()
 
@@ -10,23 +9,24 @@ DISCORD_CLIENT_TOKEN = os.getenv('DISCORD_CLIENT_TOKEN')
 SERVER_ID = os.getenv('SERVER_ID')
 CHANNEL_ID = os.getenv('CHANNEL_ID')
 
-print(f'DISCORD_CLIENT_TOKEN {DISCORD_CLIENT_TOKEN}')
-print(f'SERVER_ID {SERVER_ID}')
-print(f'CHANNEL_ID {CHANNEL_ID}')
-
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+async def scrape_news():
     ch = await client.fetch_channel(CHANNEL_ID)
-
+    stories = []
     stories = scrape_manager.scrape_all() # [(article title, article link), ...]
 
     for story in stories:
         await ch.send(content=f'{story[0]} {story[1]}')
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+    await scrape_news()
+    exit()
+    
 
 client.run(DISCORD_CLIENT_TOKEN)
